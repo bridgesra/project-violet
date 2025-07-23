ac280ad1d7111e3cf8910829d934e1bd65c2a689
danielstoopendahl <daniel.stoopendahl@gmail.com>
2025-07-23 11:30:06 -0500

    fixed python 3.11 problem

--- Blue_Lagoon/honeypot_tools.py
+++ Blue_Lagoon/honeypot_tools.py
@@ -1,16 +1,21 @@
 import subprocess
 import os
 
+runid = os.environ.get("RUNID")
+openai_key = os.environ.get("OPENAI_API_KEY")
+together_key = os.environ.get("TOGETHER_AI_SECRET_KEY")
+hp_model = os.environ.get("HP_MODEL")
+
 def start_dockers():
     print("Starting Docker containers...")
-    subprocess.run(["sudo", f"RUNID={os.environ.get("RUNID")}", f"OPENAI_API_KEY={os.environ.get("OPENAI_API_KEY")}", f"TOGETHER_AI_SECRET_KEY={os.environ.get("TOGETHER_AI_SECRET_KEY")}", f"HP_MODEL={os.environ.get("HP_MODEL")}", "docker-compose","-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"), "build"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.run(["sudo", f"RUNID={os.environ.get("RUNID")}", f"OPENAI_API_KEY={os.environ.get("OPENAI_API_KEY")}", f"TOGETHER_AI_SECRET_KEY={os.environ.get("TOGETHER_AI_SECRET_KEY")}", f"HP_MODEL={os.environ.get("HP_MODEL")}", "docker-compose", "-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"), "up", "-d"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", f"RUNID={runid}", f"OPENAI_API_KEY={openai_key}", f"TOGETHER_AI_SECRET_KEY={together_key}", f"HP_MODEL={hp_model}", "docker-compose","-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"), "build"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", f"RUNID={runid}", f"OPENAI_API_KEY={openai_key}", f"TOGETHER_AI_SECRET_KEY={together_key}", f"HP_MODEL={hp_model}", "docker-compose", "-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"), "up", "-d"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Docker containers started")
 
 
 def stop_dockers():
     print("Stopping Docker containers...")
-    subprocess.run(["sudo", f"RUNID={os.environ.get("RUNID")}", f"OPENAI_API_KEY={os.environ.get("OPENAI_API_KEY")}", f"TOGETHER_AI_SECRET_KEY={os.environ.get("TOGETHER_AI_SECRET_KEY")}", f"HP_MODEL={os.environ.get("HP_MODEL")}", "docker-compose", "-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"),"down"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["sudo", f"RUNID={runid}", f"OPENAI_API_KEY={openai_key}", f"TOGETHER_AI_SECRET_KEY={together_key}", f"HP_MODEL={hp_model}", "docker-compose", "-f", f"docker-compose.yml", "-p", os.environ.get("RUNID"),"down"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print("Docker containers stopped")
     subprocess.run(["sudo", "docker", "image", "prune", "-f"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
