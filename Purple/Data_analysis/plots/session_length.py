5bcf7c113c7aa1de4129242ab85b6367a6d81772
Sackarias Lunman <lunman@chalmers.se>
2025-07-29 14:41:33 -0500

    ifxed

--- Purple/Data_analysis/plots/session_length.py
+++ Purple/Data_analysis/plots/session_length.py
@@ -12,15 +12,15 @@ def plot_session_length(
     length_datas = [measure_session_length(sessions) for sessions, _, _ in experiments]
 
     plt.figure(figsize=(12, 6))
-    plt.ylabel("Number of commands")
     n_exps = len(experiments)
     for i, (length_data, (_, _, reconfig_indices), exp_name) in enumerate(
             zip(length_datas, experiments, experiment_names)):
-        plt.xlabel("Session")
         if subplot:
             plt.subplot(1, n_exps, i + 1)
+        plt.xlabel("Session")
+        plt.ylabel("Number of commands")
         plt.plot(length_data["session_lengths"], color=colors.scheme[i], label=exp_name)
         for index in reconfig_indices:
             plt.axvline(index - 0.5, color=colors.scheme[i], linestyle="--", alpha=0.2)
     plt.figlegend()
-
+    plt.tight_layout()
